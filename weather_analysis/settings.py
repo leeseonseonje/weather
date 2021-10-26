160e9220a31359b5c31ff622b48095414784e91a
leeseonseonje <leeseonje9323@gmail.com>
2021-10-26 23:51:46 +0900

    modulenotfounderror

--- weather_analysis/settings.py
+++ weather_analysis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g!gxa&_29x=gef6m7r(_!xjn29j@e(y@!b5690lh3s99tn3za0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -103,15 +104,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
+
 
 
 # Static files (CSS, JavaScript, Images)
